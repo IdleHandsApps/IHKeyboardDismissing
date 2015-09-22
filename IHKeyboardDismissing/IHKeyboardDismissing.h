26160c423dab14c1fc0df55ad6696f9fec2d88fe
Nathan Kot <me@nathankot.com>
2015-09-23 03:14:02 +0900

    Add Carthage support
    * Renames the existing target to IHKeyboardDismissingApp
    * Adds a new framework target called IHKeyboardDismissing
    * Shares the new framework target scheme

---
+++ IHKeyboardDismissing/IHKeyboardDismissing.h
@@ -0,0 +1,19 @@
+//
+//  IHKeyboardDismissing.h
+//  IHKeyboardDismissing
+//
+//  Created by Nathan Kot on 23/09/15.
+//  Copyright © 2015 Idle Hands Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IHKeyboardDismissing.
+FOUNDATION_EXPORT double IHKeyboardDismissingVersionNumber;
+
+//! Project version string for IHKeyboardDismissing.
+FOUNDATION_EXPORT const unsigned char IHKeyboardDismissingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IHKeyboardDismissing/PublicHeader.h>
+
+
